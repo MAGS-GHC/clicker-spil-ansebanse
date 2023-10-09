d64d648f33aa1d1d71e48c8f18792a3a814939fc
ansebanse <146828882+ansebanse@users.noreply.github.com>
2023-10-09 22:20:10 +0200

    Done for the day

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
 let banan = 0
 function minFunktion() {
-    banan += 1 
-    document.getElementById("demo").innerHTML = banan + 'Bananer';
+    banan += 1
+    if(banan === 1) {
+    }
+    document.getElementById("demo").innerHTML = banan + ' Bananer';
 }
+
+
+let monkeycost = 20
+let monkey = 0
+
+
+function BuyMonkey() {
+    if (banan >= monkeycost) {
+        banan = banan - monkeycost
+        document.getElementById("demo").innerHTML = banan + ' Bananer';
+    }
+}
+
+function addtobananer(amount) {
+    banan = banan + amount;
+    document.getElementsById("demo").innerHTML = banan + 'Bananer';
+}
+
+setInterval(function BuyMonkey(){
+    banan = banan + monkey*40;
+    document.getElementsById("demo").innerHTML = banan + 'Bananer';
+} , 1000); 
+udatepersecond 
+
+
