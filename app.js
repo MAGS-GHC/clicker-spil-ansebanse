61b43a324b9743530bb4139907f3df3059251b61
ansebanse <146828882+ansebanse@users.noreply.github.com>
2023-10-10 22:31:38 +0200

    Day 2

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
-let banan = 0
+let banan = 0;
+let havemonkey = false;
+let numberofmonkeys = 0
+
 function minFunktion() {
     banan += 1
-    if(banan === 1) {
+    if (havemonkey){
+        banan += numberofmonkeys 
+
     }
-    document.getElementById("demo").innerHTML = banan + ' Bananer';
+    if(banan === 1)
+    {
+        document.getElementById("demo").innerHTML = banan + ' Banan';  
+    }
+    else {
+    document.getElementById("demo").innerHTML = banan + ' Bananer';}
 }
 
-
-let monkeycost = 20
+let monkeycost = 10
 let monkey = 0
 
 
 function BuyMonkey() {
     if (banan >= monkeycost) {
         banan = banan - monkeycost
+        havemonkey = true 
+        numberofmonkeys++
         document.getElementById("demo").innerHTML = banan + ' Bananer';
     }
 }
-
-function addtobananer(amount) {
-    banan = banan + amount;
-    document.getElementsById("demo").innerHTML = banan + 'Bananer';
-}
-
-setInterval(function BuyMonkey(){
-    banan = banan + monkey*40;
-    document.getElementsById("demo").innerHTML = banan + 'Bananer';
-} , 1000); 
-udatepersecond 
-
-
